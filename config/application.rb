3987b0faf3a6dc1781f12a089c68fd9536e4143f
Robert Rice <rarice@g.cofc.edu>
2014-10-23 12:22:42 -0400

    af

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module FinalProject
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.serve_static_assets = true
+    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+    config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
