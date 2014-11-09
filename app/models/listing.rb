adc0cf660cc27dd42a73bd2013df791294b4a164
Robert Rice <rarice@g.cofc.edu>
2014-11-09 11:36:36 -0500

    remove fropbox from listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,13 +1,13 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :photo,
-    :storage => :dropbox,
-    :dropbox_credentials => { app_key: ENV['APP_KEY'],
-                              app_secret: ENV['APP_SECRET'],
-                              access_token: ENV['ACCESS_TOKEN'],
-                              access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
-                              user_id: ENV['USER_ID'],
-                              access_type: 'app_folder'}
-  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :photo
+  #   :storage => :dropbox,
+  #   :dropbox_credentials => { app_key: ENV['APP_KEY'],
+  #                             app_secret: ENV['APP_SECRET'],
+  #                             access_token: ENV['ACCESS_TOKEN'],
+  #                             access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+  #                             user_id: ENV['USER_ID'],
+  #                             access_type: 'app_folder'}
+  # validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   belongs_to :user
   geocoded_by :full_street_address  
   after_validation :geocode
