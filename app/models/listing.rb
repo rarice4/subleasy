fe938cb91a0263ec0352b7bf5747ac326d7d050b
Robert Rice <rarice@g.cofc.edu>
2014-11-09 21:17:19 -0500

    remove :url =>

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :photo, :s3_protocol => :https, :url => ":s3_domain_url"
+  has_attached_file :photo, :s3_protocol => :https
   #   :storage => :dropbox,
   #   :dropbox_credentials => { app_key: ENV['APP_KEY'],
   #                             app_secret: ENV['APP_SECRET'],
