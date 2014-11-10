1886f83f8e862b9a312639ba7eed2c12b70f2219
Robert Rice <rarice@g.cofc.edu>
2014-11-10 12:28:26 -0500

    restore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,6 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      :region => "us-west-2"
     }
   } 
 end
