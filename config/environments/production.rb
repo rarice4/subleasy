8cf384985fa4c51ef2389048d11245ad73f661a1
Robert Rice <rarice@g.cofc.edu>
2014-11-11 11:48:19 -0500

    more info logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-  } 
+  }
+
+  config.logger = Logger.new(STDOUT) 
 end
